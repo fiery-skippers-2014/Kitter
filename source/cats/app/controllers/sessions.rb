a3867decf50aab70601e2615111973a13f766385
Andrew <jdrewsdc@gmail.com>
2014-04-03 16:43:44 -0700

    Controller

--- source/cats/app/controllers/sessions.rb
+++ source/cats/app/controllers/sessions.rb
@@ -1,7 +1,3 @@
-
-
-enable :sessions
-
 get '/' do
   if session[:user_id] != nil # for users already logged in
     redirect "/#{User.find(session[:user_id]).user_name}"
@@ -10,20 +6,55 @@ get '/' do
   end
 end
 
-post '/' do
-  @current_user = current_user("user_name", params[:user_name])
-  if valid_user?(params[:password], @current_user.password)
-    redirect '/:your_page'
+post '/welcome_back' do
+  @user = User.find_by_user_name(params[:user_name])
+  if User.authenticate(params[:user_name], params[:password])
+    session[:user_id]=@user.id
+    @all_users = User.all
+    @all_users.delete(session[:user_id])
+    @all_followings = Following.find_by_user_id(session[:user_id])
+    @all_follower = Follower.find_by_user_id(session[:user_id])
+    erb :yourpage
   else
-    redirect '/'
+    @fail = "Your password or username was incorrect. Try again"
+    erb :index
   end
 end
 
-get '/:user_name' do
-  # render appropriate view
-
-  erb :your_page
+post '/register' do
+  @user = User.create(full_name: params[:full_name], user_name: params[:user_name],  password: params[:password] )
+  if @user.id == nil
+    @fail = "You can't register with that info. Try again"
+    erb :index
+  else
+    session[:user_id]=@user.id
+    @all_users = User.all
+    @all_users.delete(session[:user_id])
+    erb :yourpage
+  end
 end
+
+
+post '/tweet/new' do
+ if Tweet.create(text: params[:tweet], user_id: sessions[:user_id]).valid?
+  redirect '/:yourpage'
+else
+  @fail = "That tweet was too terrible"
+  erb :yourpage
+ end
+end
+
+post '/followers/new' do
+if Follower.create(user_name: params[:follower], user_id: sessions[:user_id]).valid?
+  redirect '/:yourpage'
+else
+  @fail = "That tweet was too terrible"
+  erb :yourpage
+ end
+end
+
+
+
 
 get '/logout' do
   session[:user_id]=nil
@@ -33,11 +64,25 @@ end
 
 # Helper methods
 
-def current_user(property, query)
-  User.where("#{property} = ?", query).first
-end
+# def current_user(property, query)
+#   User.where("#{property} = ?", query).first
+# end
 
-def valid_user?(params_password, actual_password) # BUGBUG
-  # needs to also check passed in username
-  params_password == actual_password
-end
+# def valid_user?(params_password, actual_password) # BUGBUG
+#   user = User.find_by_user_name(user_name)
+#       if user != nil
+#   params_password == actual_password
+# end
+
+
+# <!-- List of people following you?
+# <% #@all_followings.each do |person| %>
+#   <%= person.user_name%>
+#   <br>
+# <% #end %>
+
+# List of people you follow?
+# <% #@all_followers.each do |person| %>
+#   <%= person.user_name%>
+#   <br>
+# <% #end %> -->
--- source/cats/db/migrate/4_create_followings.rb
+++ source/cats/db/migrate/4_create_followings.rb
@@ -1,8 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |col|
-      col.belongs_to :user
-      col.string :user_name
+      col.belongs_to  :user
+      col.string      :user_name
 
       col.timestamps
     end
