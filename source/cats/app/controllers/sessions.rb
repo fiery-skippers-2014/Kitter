a3b028c77af19de6e44e7121bd2ad17da72cba4b
Andrew <jdrewsdc@gmail.com>
2014-04-03 16:50:43 -0700

    Release -11

--- source/cats/app/controllers/sessions.rb
+++ source/cats/app/controllers/sessions.rb
@@ -52,6 +52,22 @@ else
  end
 end
 
+get '/news_feed' do
+  # User logged in
+  if session[:user_id] != nil
+    @user = User.find_by_id(session[:user_id])
+    erb: news_feed
+
+  # User not logged in
+  else
+    @fail = "You are not logged in! Please log in."
+    erb: index
+  end
+
+end
+
+
+
 get '/logout' do
   session[:user_id]=nil
   redirect ('/')
