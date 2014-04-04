f830f016ea608e05f32e007072c7113dcd51789b
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-04-03 21:20:13 -0700

    added seed users for the sake of testing, and replaced gravatar img links for the fake users with placekitten img links for demo

--- db/migrate/1_create_users.rb
+++ db/migrate/1_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |col|
-    col.string :full_name
-    col.string :user_name
-    col.string :email
-    col.string :password
+      col.string :full_name
+      col.string :user_name
+      col.string :email
+      col.string :password
 
-    col.timestamps
+      col.timestamps
     end
   end
 end
